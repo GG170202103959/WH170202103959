5f3e46d532a5500ccf66dbd60bfbb0757a7a61f8
Administrator <Administrator@MD501-87>
2019-05-24 13:31:53 +0800

    03

---
+++ src/com/bl/student.java
@@ -0,0 +1,13 @@
+package com.bl;
+
+public class student {
+	String codeNumber;
+	String name;
+	student(){
+		
+	}
+	student(String codeNumber, String name){
+		this.codeNumber=codeNumber;
+		this.name=name;
+	}
+}
---
+++ src/com/bl/teacher.java
@@ -0,0 +1,13 @@
+package com.bl;
+
+public class teacher {
+	String codeNumber;
+	String name;
+	teacher(){
+		
+	}
+	teacher(String codeNumber, String name){
+		this.codeNumber=codeNumber;
+		this.name=name;
+	}
+}
